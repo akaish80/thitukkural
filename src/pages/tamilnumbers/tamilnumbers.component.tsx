67cb89c72c755786748a6c7bb25fb9ee97688a23
Arunkumar Dharmarajan <akaish@gmail.com>
2026-04-22 12:28:45 -0400

    update leearning path

--- src/pages/tamilnumbers/tamilnumbers.component.tsx
+++ src/pages/tamilnumbers/tamilnumbers.component.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { speakTamil as speakTamilText } from '../../utils/pronunciationEngine';
+import { speakTamil, speakTamil as speakTamilText } from '../../utils/pronunciationEngine';
 import './tamilnumbers.styles.scss';
 
 interface TamilNumber {
